6d8e83e3ff91bf699e4993cedc739436c409bd62
K Gunasekhar <gunasekhark29@gmail.com>
2025-08-06 21:47:52 +0530

    Initial commit

---
+++ sum_app/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def sum_view(request):  
+    if request.method == 'POST':
+        num1 = int(request.POST.get('num1', 0))
+        num2 = int(request.POST.get('num2', 0))
+        result = num1 + num2
+        return HttpResponse(f"మొత్తం: {result}")
+    template = loader.get_template('sum.html')
+    return HttpResponse(template.render({}, request))
